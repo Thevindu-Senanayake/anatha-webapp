5ad2bca343b22447144357631fae98ebf40eb899
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-11 00:17:28 +0530

    Feature Added: Search Product by Keyword

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 // Create new product  => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors (async (req, res, next) => {
@@ -13,10 +14,13 @@ exports.newProduct = catchAsyncErrors (async (req, res, next) => {
 	})
 })
 
-// Get All products  => /api/v1/products
+// Get All products  => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors (async (req, res, next) => {
 
-	const products = await Product.find();
+	const apiFeatures = new APIFeatures(Product.find(), req.query)
+		.search()
+
+	const products = await apiFeatures.query;
 
 	res.status(200).json({
 		success: true,
