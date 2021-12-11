b426956fc711a0b9af9e63d6e6919564a762e558
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-11 15:35:13 +0530

    Encrypted Password while Registration

---
+++ backend/controllers/authController.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+
+// Register a user  => /api/v1/register
+exports.registerUser = catchAsyncErrors (async (req, res, next) => {
+
+	const { name, email, password } = req.body;
+
+	const user = await User.create({
+		name,
+		email,
+		password
+	});
+
+	res.status(201).json({
+		success: true,
+		user
+	})
+})
