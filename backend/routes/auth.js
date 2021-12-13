fedbced831fc39d2c96fd3ac6d2fc982322650f1
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-13 17:39:02 +0530

    Feature Added: Send Recovery Email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logoutUser } = require('../controllers/authController');
+const { registerUser, loginUser, forgotPassword, logoutUser } = require('../controllers/authController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/password/forgot').post(forgotPassword);
+
 router.route('/logout').get(logoutUser);
 
 module.exports = router;
