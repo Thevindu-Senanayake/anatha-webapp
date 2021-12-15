cb9c624871d3ab12ff5f65da5428bbf72fc64746
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-16 03:48:11 +0530

    Route Added: Get Single Order & Current User's Orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,29 @@ exports.newOrder = catchAsyncErrors( async (req, res, next) => {
 		order
 	})
 })
+
+
+// Get single order	=> /api/v1/order/:id
+exports.getSingleOrderDetails = catchAsyncErrors( async (req, res, next) => {
+	const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+	if (!order) {
+		return next(new ErrorHandler('no order found with this ID', 404));
+	}
+
+	res.status(200).json({
+		success: true,
+		order
+	})
+})
+
+
+// Get logged in user's all orders	=> /api/v1/orders/me
+exports.myOrders = catchAsyncErrors( async (req, res, next) => {
+	const orders = await Order.find({ user: req.user.id });
+
+	res.status(200).json({
+		success: true,
+		orders
+	})
+})
