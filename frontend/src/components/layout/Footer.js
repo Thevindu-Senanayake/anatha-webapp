f1eaf5adc72523b6e63a220ad8141630678948e1
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-17 16:33:03 +0530

    Home Page

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 const Footer = () => {
-	return (
-		<Fragment>
-			<footer className="py-1">
-      		<p className="text-center text-white mt-1">
-        			Shopping Cart - 2019-2020, All Rights Reserved
-      		</p>
-    		</footer>
-		</Fragment>
-	)
-}
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          anatha web app - 12/17/2021, All Rights Reserved
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
 
-export default Footer
+export default Footer;
