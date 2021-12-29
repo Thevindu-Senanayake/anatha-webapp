5864a83b638d1d9cc2106d7ff2b12c5c3ca7d530
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2021-12-30 04:21:00 +0530

    Filter By Ratings

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,33 +10,33 @@ import {
 	CLEAR_ERRORS,
 } from "../constants/productConstants";
 
-export const getProducts = (keyword = '', currentPage = 1, price, category) => async (dispatch) => {
-	try {
-		dispatch({ type: ALL_PRODUCTS_REQUEST });
+export const getProducts =
+	(keyword = "", currentPage = 1, price, category, rating = 0) =>
+	async (dispatch) => {
+		try {
+			dispatch({ type: ALL_PRODUCTS_REQUEST });
 
-		let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}`
+			let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&ratings[gte]=${rating}`;
 
-		if (category) {
-			link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}`
+			if (category) {
+				link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&ratings[gte]=${rating}&category=${category}`;
+			}
+
+			const { data } = await axios.get(link);
+
+			dispatch({
+				type: ALL_PRODUCTS_SUCCESS,
+				payload: data,
+			});
+		} catch (error) {
+			dispatch({
+				type: ALL_PRODUCTS_FAIL,
+				payload: error.response.data.message,
+			});
+
+			console.log(error);
 		}
-
-		console.log(category);
-		console.log(link);
-		const { data } = await axios.get(link);
-
-		dispatch({
-			type: ALL_PRODUCTS_SUCCESS,
-			payload: data,
-		});
-	} catch (error) {
-		dispatch({
-			type: ALL_PRODUCTS_FAIL,
-			payload: error.response.data.message,
-		});
-
-		console.log(error);
-	}
-};
+	};
 
 export const getProductDetails = (id) => async (dispatch) => {
 	try {
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,8 @@
 import React, { Fragment, useState, useEffect } from "react";
-import Pagination from 'react-js-pagination';
-import { useParams } from 'react-router-dom';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import Pagination from "react-js-pagination";
+import { useParams } from "react-router-dom";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 import MetaData from "./layout/MetaData";
 import Product from "./product/Product";
@@ -12,54 +12,58 @@ import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
 import { getProducts } from "../actions/productActions";
 
-const { createSliderWithTooltip } = Slider
-const Range = createSliderWithTooltip(Slider.Range)
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
 
 const Home = () => {
-
-	const [currentPage, setCurrentPage] = useState(1)
-	const [price, setPrice] = useState([1, 1000])
-	const [category, setCategory] = useState('')
+	const [currentPage, setCurrentPage] = useState(1);
+	const [price, setPrice] = useState([1, 1000]);
+	const [category, setCategory] = useState("");
+	const [rating, setRating] = useState(0);
 
 	const categories = [
-		'books',
-		'pencils',
-		'cuters',
-		'pastles',
-		'Electronics',
-		'Headphones',
-		'Accessories',
-		'Cameras',
-		'Laptops',
-		'Food'
-	]
+		"books",
+		"pencils",
+		"cuters",
+		"pastles",
+		"Electronics",
+		"Headphones",
+		"Accessories",
+		"Cameras",
+		"Laptops",
+		"Food",
+	];
 
 	const alert = useAlert();
 	const dispatch = useDispatch();
 	const params = useParams();
 
-	const { loading, products, error, productCount, resPerPage, filteredProductCount } = useSelector(
-		(state) => state.products
-	);
+	const {
+		loading,
+		products,
+		error,
+		productCount,
+		resPerPage,
+		filteredProductCount,
+	} = useSelector((state) => state.products);
 
-	const keyword = params.keyword
+	const keyword = params.keyword;
 
 	useEffect(() => {
 		if (error) {
 			return alert.error(error);
 		}
 
-		dispatch(getProducts(keyword, currentPage, price, category));
-
-	}, [dispatch, alert, error, keyword, currentPage, price, category]);
+		dispatch(getProducts(keyword, currentPage, price, category, rating));
+	}, [dispatch, alert, error, keyword, currentPage, price, category, rating]);
 
 	function setCurrentPageNo(pageNumber) {
-		setCurrentPage(pageNumber)
+		setCurrentPage(pageNumber);
 	}
 
 	let count = productCount;
 	if (keyword) {
-		count = filteredProductCount
+		count = filteredProductCount;
 	}
 
 	return (
@@ -81,38 +85,66 @@ const Home = () => {
 											<Range
 												marks={{
 													1: `$1`,
-													1000: `$1000`
+													1000: `$1000`,
 												}}
 												min={1}
 												max={1000}
 												defaultValue={[1, 1000]}
-												tipFormatter={value => `$${value}`}
+												tipFormatter={(value) => `$${value}`}
 												tipProps={{
 													placement: "top",
-													visible: true
+													visible: true,
 												}}
 												value={price}
-												onChange={price => setPrice(price)}
+												onChange={(price) => setPrice(price)}
 											/>
 
 											<hr className="my-5" />
 
 											<div className="mt-5">
-												<h4 className="mb-3">
-													Categories
-												</h4>
+												<h4 className="mb-3">Categories</h4>
 
 												<ul className="pl-0">
-													{categories.map(category => (
+													{categories.map((category) => (
 														<li
 															style={{
 																cursor: "pointer",
-																listStyleType: "none"
+																listStyleType: "none",
 															}}
 															key={category}
-															onClick={() => setCategory(category)}
+															onClick={() =>
+																setCategory(category)
+															}
 														>
 															{category}
+														</li>
+													))}
+												</ul>
+											</div>
+
+											<hr className="my-3" />
+
+											<div className="mt-5">
+												<h4 className="mb-3">Ratings</h4>
+
+												<ul className="pl-0">
+													{[5, 4, 3, 2, 1].map((star) => (
+														<li
+															style={{
+																cursor: "pointer",
+																listStyleType: "none",
+															}}
+															key={star}
+															onClick={() => setRating(star)}
+														>
+															<div className="rating-outer">
+																<div
+																	className="rating-inner"
+																	style={{
+																		width: `${star * 20}%`,
+																	}}
+																></div>
+															</div>
 														</li>
 													))}
 												</ul>
@@ -123,17 +155,24 @@ const Home = () => {
 									<div className="col-6 col-md-9">
 										<div className="row">
 											{products.map((product) => (
-												<Product key={product._id} product={product} col={4} />
+												<Product
+													key={product._id}
+													product={product}
+													col={4}
+												/>
 											))}
 										</div>
 									</div>
 								</Fragment>
 							) : (
 								products.map((product) => (
-									<Product key={product._id} product={product} col={3} />
+									<Product
+										key={product._id}
+										product={product}
+										col={3}
+									/>
 								))
 							)}
-
 						</div>
 					</section>
 
@@ -144,10 +183,10 @@ const Home = () => {
 								itemsCountPerPage={resPerPage}
 								totalItemsCount={productCount}
 								onChange={setCurrentPageNo}
-								nextPageText={'Next'}
-								prevPageText={'Prev'}
-								firstPageText={'First'}
-								lastPageText={'Last'}
+								nextPageText={"Next"}
+								prevPageText={"Prev"}
+								firstPageText={"First"}
+								lastPageText={"Last"}
 								itemClass="page-item"
 								linkClass="page-link"
 							/>
