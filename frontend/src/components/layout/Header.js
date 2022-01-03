d348658f9d035375e76d2f6840d51d13cc0a0e68
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2022-01-03 23:17:04 +0530

    Show User In Header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -2,9 +2,17 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+
 import Search from "./Search";
 
 const Header = () => {
+	const alert = useAlert();
+	const dispatch = useDispatch();
+
+	const { user, loading } = useSelector((state) => state.auth);
+
 	return (
 		<Fragment>
 			<nav className="navbar row">
@@ -21,15 +29,67 @@ const Header = () => {
 				</div>
 
 				<div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-					<Link to="/login" className="btn ml-4" id="login_btn">
-						Login
+					{user ? (
+						<div className="ml-4 dropdown d-inline">
+							<Link
+								to="#!"
+								className="btn dropdown-toggle text-white"
+								type="button"
+								id="dropDownMenuButton"
+								data-toggle="dropdown"
+								aria-expanded="false"
+								aria-haspopup="true"
+							>
+								<figure className="avatar avatar-nav">
+									<img
+										src=""
+										// alt={user && user.name}
+										className="rounded-circle"
+									/>
+								</figure>
+								<span>{user && user.name}</span>
+							</Link>
+							<div
+								className="dropdown-menu"
+								aria-labelledby="dropDownMenuButton"
+							>
+								{user && user.role !== "admin" ? (
+									<Link className="dropdown-item" to="/orders/me">
+										My Orders
+									</Link>
+								) : (
+									<Link className="dropdown-item" to="/dashboard">
+										Dashboard
+									</Link>
+								)}
+
+								<Link className="dropdown-item" to="/orders/me">
+									My Orders
+								</Link>
+								<Link className="dropdown-item" to="/me">
+									My Account
+								</Link>
+								<Link to="/" className="dropdown-item text-danger">
+									Logout
+								</Link>
+							</div>
+						</div>
+					) : (
+						!loading && (
+							<Link to="/login" className="btn ml-4" id="login_btn">
+								Login
+							</Link>
+						)
+					)}
+
+					<Link to="/cart" style={{ textDecoration: "none" }}>
+						<span id="cart" className="ml-3">
+							Cart
+						</span>
+						<span className="ml-1" id="cart_count">
+							2
+						</span>
 					</Link>
-					<span id="cart" className="ml-3">
-						Cart
-					</span>
-					<span className="ml-1" id="cart_count">
-						2
-					</span>
 				</div>
 			</nav>
 		</Fragment>
