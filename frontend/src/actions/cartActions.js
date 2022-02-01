bef5b2656fd6a38830058034220c2157154b85f2
Thevindu-Senanayake <thevindusenanayake76@gmail.com>
2022-02-01 15:46:09 +0530

    Fixed Bug Add to Cart Button

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { ADD_TO_CART } from "../constants/cartConstants";
 
-export const addItemToCart = (id, quantity) => (dispatch, getState) => {
-	const { data } = axios.get(`api/v1/product/${id}`);
+export const addItemToCart = (id, quantity) => async (dispatch, getState) => {
+	const { data } = await axios.get(`/api/v1/product/${id}`);
 
 	dispatch({
 		type: ADD_TO_CART,
 		payload: {
-			product: data.product_id,
+			product: data.product._id,
 			name: data.product.name,
 			price: data.product.price,
 			image: data.product.images[0].url,
